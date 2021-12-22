adef2d635325aeccb6496bef676ebc5002664a90
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-12-22 20:32:26 +0900

    refactor BUILDARGS

--- lib/Data/Gimei/Word.pm
+++ lib/Data/Gimei/Word.pm
@@ -16,16 +16,18 @@ around BUILDARGS => sub {
     my $orig  = shift;
     my $class = shift;
 
+    my %args;
     if ( 'ARRAY' eq ref $_[0] ) {
-        return $class->$orig( kanji    => $_[0]->[0],
-                              hiragana => $_[0]->[1],
-                              katakana => $_[0]->[2],
-                              romaji   => ucfirst $_[0]->[3] );
+        %args = ( kanji    => $_[0]->[0],
+                  hiragana => $_[0]->[1],
+                  katakana => $_[0]->[2],
+                  romaji   => $_[0]->[3] );
     } else {
-        my %args = @_;
-        $args{'romaji'} = ucfirst( $args{'romaji'} );
-        return $class->$orig(%args);
+        %args = @_;
     }
+
+    $args{'romaji'} = ucfirst( $args{'romaji'} );
+    return $class->$orig(%args);
 };
 
 1;
