fe6cd663e4bfe2762b42812b3067ee9655462805
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-12-22 16:34:42 +0900

    add authoring tool

--- author/reduce_kana.pl
+++ author/reduce_kana.pl
@@ -1,29 +1,31 @@
 #!/usr/bin/env perl
 
+# cut katakana from yaml
+
 use strict;
 use warnings;
 
 use English;
 use utf8;
+use open IO => ":utf8";
 binmode STDOUT, ":utf8";
 use feature ':5.30';
 
-
-#use File::Path qw(make_path);
 use File::Spec;
-#use YAML::XS;
-use YAML;
 
 # read yml
-my $yaml_path = File::Spec->catfile('share', 'names1.yml');
-my $names = YAML::LoadFile($yaml_path);
+my $yaml_path = File::Spec->catfile('share', 'names.yml');
+open FH, "<", $yaml_path
+    or die "Can't open < $yaml_path: $!";
 
-# cut column
-say $names->{'last_name'}->[0]->[0];
-my $name = $names->{'last_name'}->[0];
-say @$name;
-pop( @$name );
-say @$name;
+# cut last column
+while (<FH>) {
+    chomp;
+    if (/:$/) {
+	print $_ . "\n";
+	next;
+    }
 
-# store yml
-YAML::DumpFile($yaml_path, $names);
+    s/,[^,]+$/]/;
+    print $_ . "\n";
+}
