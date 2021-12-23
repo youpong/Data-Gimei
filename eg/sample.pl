b1f137aa8141368153f1cd262717f2b9a9c1834b
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-12-23 21:08:14 +0900

    rename var name.

--- eg/sample.pl
+++ eg/sample.pl
@@ -7,19 +7,21 @@ binmode STDOUT, ":utf8";
 use feature ':5.30';
 
 use Data::Gimei;
+my $name = Data::Gimei::Name->new();
+                                 # for example
+say $name->kanji;                # "斎藤陽菜"
+say $name->hiragana;             # "さいとうはるな"
+say $name->katakana;             # "サイトウハルナ"
+say $name->romaji;               # "Haruna Saito"
 
-my $gimei = Data::Gimei::Name->new();
-say $gimei->kanji;                # "斎藤陽菜"
-say $gimei->hiragana;             # "さいとうはるな"
-say $gimei->katakana;             # "サイトウハルナ"
-say $gimei->romaji;               # "Haruna Saito"
+say $name->last_name->kanji;     # "斎藤"
+say $name->last_name->hiragana;  # "さいとう"
+say $name->last_name->katakana;  # "サイトウ"
+say $name->last_name->romaji;    # "Saito"
 
-say $gimei->last_name->kanji;     # "斎藤"
-say $gimei->last_name->hiragana;  # "さいとう"
-say $gimei->last_name->katakana;  # "サイトウ"
-say $gimei->last_name->romaji;    # "Saito"
+say $name->first_name->kanji;    # "陽菜"
+say $name->first_name->hiragana; # "はるな"
+say $name->first_name->katakana; # "ハルナ"
+say $name->first_name->romaji;   # "Haruna"
 
-say $gimei->first_name->kanji;    # "陽菜"
-say $gimei->first_name->hiragana; # "はるな"
-say $gimei->first_name->katakana; # "ハルナ"
-say $gimei->first_name->romaji;   # "Haruna"
+say $name->gender;               # "female"
