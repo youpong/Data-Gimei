1e4cb730e4c567f38c0c86305a6ce181747cbc82
NAKAJIMA Yusaku <yupong7@gmail.com>
2026-01-16 12:20:30 +0900

    Update License

--- lib/Data/Gimei.pm
+++ lib/Data/Gimei.pm
@@ -144,7 +144,10 @@ A command-line tool for this module.
 
 =head1 LICENSE
 
-MIT License
+Copyright (c) 2021-2026 Yusaku Nakajima.
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the MIT License.
 
 Dictionary YAML file is generated from naist-jdic.
 
